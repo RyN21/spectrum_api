0f2e3fdd867215147821f2eb70c1fb9129b97fc0
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-25 20:01:04 -0600

    Add request to call api from energy.gov

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 import flask
+import requests
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>It works!</h1>"
-
+    r = requests.get("https://www.energy.gov/sites/prod/files/2020/12/f81/code-12-15-2020.json")
+    json = r.json()
+    return json
 app.run()
