345096bd4a6b210b4d62d94696226ec1e3194b4d
ryn21 <33795357+RyN21@users.noreply.github.com>
2021-06-28 15:29:12 -0600

    Write CSV files from given data. CSV files contain only relevent data for this exercise.

--- api.py
+++ api.py
@@ -1,12 +1,33 @@
 import flask
 import requests
+import csv
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+
+# CREATE RELEASE JSON FROM API REQUEST
+r = requests.get("https://www.energy.gov/sites/prod/files/2020/12/f81/code-12-15-2020.json")
+json = r.json()
+releases = json["releases"]
+
+
+# WRITE CSV FILE
+with open('organizations.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['organization', 'labor_hours', 'status', 'licenses', 'date_created'])
+
+    for x in releases:
+        licenses = []
+        for l in x['permissions']['licenses']:
+            name = l['name']
+            licenses.append(name)
+
+        writer.writerow([x['organization'], x['laborHours'], x['status'], licenses, x['date']['created']])
+
+
 @app.route('/', methods=['GET'])
+
 def home():
-    r = requests.get("https://www.energy.gov/sites/prod/files/2020/12/f81/code-12-15-2020.json")
-    json = r.json()
-    return json
+    return releases[0]
 app.run()
